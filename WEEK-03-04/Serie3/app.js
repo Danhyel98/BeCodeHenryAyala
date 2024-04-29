956d80ecd82975fdea2d93cc8e5a1557c837a61d
Henry <ayala.hd98@gmail.com>
2024-04-29 19:15:19 +0200

    29 AVRIL 2024

--- WEEK-03-04/Serie3/app.js
+++ WEEK-03-04/Serie3/app.js
@@ -64,12 +64,67 @@ console.log(reverseOdd("Bananas"));
 console.log(reverseOdd("One two three four"));
 console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));*/
 
+//EXERCICE 03
+/*function isSmooth(word) {
+  let lowWord= word.toLowerCase().split(' ');
+    for (let i = 0; i<lowWord.length-1; i++) {
+      let firstLetter = lowWord[i];
+      let lastLetter = lowWord[i+1];
+      //return firstLetter[firstLetter.length-1];
+      //return lastLetter[0];
+        if (firstLetter[firstLetter.length-1] === lastLetter[0]) {
+          return true;
+  } 
+}return false;
+} 
 
-/* JE ME SUIS ARRETE ICI j ai pas encore terminé
-function isSmooth(str) {
-    let allLc = str.toLowerCase();
-    let newArr = allLc.split(" ");
-    if (newArr[i].length-1 )
-    return newArr;
+console.log(isSmooth("Marta appreciated deep perpendicular right trapezoids"));
+console.log(isSmooth("Someone is outside the doorway")); 
+console.log(isSmooth("She eats super righteously"));*/
+/*
+// EXERCICE 04
+function sevenBoom(dig) {
+  let newDig = dig.join("").split("").map(Number);
+  //return newDig;
+    if (newDig.includes(7)) {
+      return "Boom!";
+    } return "There'is no 7 in this array";
 }
-console.log(isSmooth("Marta appreciated deep perpendicular right trapezoids"));*/
+console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
+console.log(sevenBoom([8, 6, 33, 100]));
+console.log(sevenBoom([2, 55, 60, 97, 86]));*/
+
+//EXERCICE 05
+// formula = (0 °C × 9/5) + 32 = 32 °F
+/*function convert(deg) {
+  let lowDeg = deg.toLowerCase();
+  if (lowDeg.includes("c")) {
+    let degr = parseInt(lowDeg);
+    return (degr * 9/5) +32;
+   //return "C is there !!"
+  }  else if (lowDeg.includes("f")) {
+    let degr = parseInt(lowDeg);
+    return ((degr-32) * 5/9).toFixed(0);
+  } return "error";
+} 
+console.log(convert("35°C"));
+console.log(convert("19°F"));
+console.log(convert("33"));*/
+
+//EXERCICE 06
+/*function findBrokenKeys (a, b) {
+  let aLow = a.toLowerCase();
+  let bLow= b.toLowerCase();
+  //let str1 = a.replace(" ","");
+  //let str2 = b.replace(" ","");
+  let bk= [];
+  for (let i =0; i < a.length; i++) {
+    if (aLow[i] !== bLow[i] && !bk.includes(aLow[i])) {
+      bk.push(aLow[i]);
+    }
+  }return bk;
+}
+//console.log(findBrokenKeys("happy birthday", "happy birthday"));
+console.log(findBrokenKeys("haPPy birthday", "haWWy birthday"));
+console.log(findBrokenKeys("starry night", "starrq light"));
+console.log(findBrokenKeys("beethoven", "affthoif5"));*/
---
+++ WEEK-03-04/Serie4/app.js
@@ -0,0 +1,10 @@
+//EXERCICE 00
+// JE SUIS ICI J AI PAS ENCORE TERMINE
+/*function listNum (n) {
+   let arrN = [];
+    for (let i =0; i< n.length-1; i++) {
+       arrN.push(n[i]);
+        
+    }  return arrN;
+}
+console.log(listNum(20))*/
