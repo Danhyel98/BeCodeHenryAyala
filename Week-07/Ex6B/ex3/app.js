c2ad8bf61a95fe4127cb72f0d958fc7d6d5f9a0d
Henry AYALA <ayala.hd98@gmail.com>
2024-05-28 12:03:38 +0200

    week07_6b_ex4

---
+++ Week-07/Ex6B/ex3/app.js
@@ -0,0 +1,5 @@
+const list= document.querySelectorAll("li").forEach(li => li.classList.toggle("highlight"));
+
+//list.forEach(li => li.classList.toggle("highlight"));
+
+//.classList.toggle("highlight");
